643d76de84221a8f5c07955461a1883ce77772af
Katalina Guerrero <katest2599@gmail.com>
2025-10-15 10:00:12 -0300

    feat: added sql output

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
-from utils.file_utils import read_valid_users
+from utils.file_utils import read_valid_users, write_sql_file_profile_users
 from utils.db_utils import get_connection, get_users_not_in_csv
 
 def main():
     conn = get_connection()
     valid_users = read_valid_users("data/valid_users.csv")
     invalid_users = get_users_not_in_csv(conn, valid_users)
-    for user in invalid_users:
-      print(f" - {user}")
+    write_sql_file_profile_users(invalid_users)
 
     conn.close()
 
--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -3,3 +3,13 @@ import pandas as pd
 def read_valid_users(csv_path: str) -> list[str]:
     df = pd.read_csv(csv_path, header=None, names=["username"])
     return df["username"].dropna().tolist()
+
+def write_sql_file_profile_users(invalid_users: list[str], output_path: str = "./SQL/SIADUS_TBACCPRO.sql") -> None:
+    with open(output_path, "w", encoding="utf-8") as outfile:
+        placeholders = ",".join("?" for _ in invalid_users)
+        sql = f"""
+            SELECT CODIGO_ADI
+            FROM SIADUS_TBACCPRO
+            WHERE CODIGO_ADI IN ({placeholders})
+        """
+        outfile.write(sql)
